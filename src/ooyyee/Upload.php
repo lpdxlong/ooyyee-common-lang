c18211a0bc5c12b18d238374984511bdfa527b41
lpdx111 <1581489696@qq.com>
2019-11-04 13:26:19 +0800

    mt_getrandmax 修改上传

--- src/ooyyee/Upload.php
+++ src/ooyyee/Upload.php
@@ -39,7 +39,7 @@ class Upload
         $format = str_replace(['{yyyy}','{yy}','{mm}','{dd}','{hh}','{ii}','{ss}','{time}'], [$d[0],$d[1],$d[2],$d[3], $d[4],$d[5],$d[6],$t], $format);
 
         //替换随机字符串
-        $randNum = mt_rand(1, 10000000000) . mt_rand(1, 10000000000);
+        $randNum = mt_rand(1, mt_getrandmax()) . mt_rand(1, mt_getrandmax());
         if (preg_match('/\{rand\:([\d]*)\}/i', $format, $matches)) {
             $format = preg_replace('/\{rand\:[\d]*\}/i', substr($randNum, 0, $matches[1]), $format);
         }
